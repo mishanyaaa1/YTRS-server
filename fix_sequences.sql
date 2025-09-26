80e834e455d930a37743ee0059441b730c94d89f
mishanyaaa1 <i.am31827@gmail.com>
2025-09-26 19:31:56 +0300

    clear project

--- fix_sequences.sql
+++
@@ -1,3 +0,0 @@
--- Исправление последовательностей для типов вездеходов и местности
-SELECT setval('terrain_types_id_seq', (SELECT max(id) FROM terrain_types));
-SELECT setval('vehicle_types_id_seq', (SELECT max(id) FROM vehicle_types));
