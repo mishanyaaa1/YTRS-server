1efbe0c38e74e009bd4cbab80de116e2573c4715
mishanyaaa1 <i.am31827@gmail.com>
2025-10-29 21:34:02 +0300

    Revert "v2"
    This reverts commit a5769a635f65adf92481b8a80f6b70f9af5a992f.

--- src/pages/VehicleDetailPage.jsx
+++ src/pages/VehicleDetailPage.jsx
@@ -184,7 +184,7 @@ function VehicleDetailPage() {
                         style={{
                           width: '100%',
                           height: '100%',
-                          objectFit: 'cover',
+                          objectFit: 'contain',
                           borderRadius: '14px',
                           cursor: 'pointer'
                         }}
